46ce2d82df5a5c931b771f78b2429f7c25327b33
Ben Fidge <ben.fidge@btopenworld.com>
2019-09-27 09:46:37 +0700

    Changed from PR https://github.com/CluedIn-io/CluedIn/pull/319/files

--- src/CrunchbasePeopleExternalSearchProvider.cs
+++ src/CrunchbasePeopleExternalSearchProvider.cs
@@ -20,7 +20,7 @@ using CluedIn.ExternalSearch.Filters;
 using CluedIn.ExternalSearch.Providers.Crunchbase.Vocabularies;
 using RestSharp;
 using CluedIn.Crawling.Helpers;
-using ExternalSearch.Providers.Crunchbase.Model;
+using CluedIn.ExternalSearch.Providers.Crunchbase.Model;
 using CluedIn.Core.Utilities;
 using CluedIn.Core.FileTypes;
 
@@ -70,7 +70,7 @@ namespace CluedIn.ExternalSearch.Providers.Crunchbase
 
             var existingResults = request.GetQueryResults<PersonProperties>(this).ToList();
 
-            Func<string, bool> nameFilter = value => existingResults.Any(r => string.Equals(r.Data.first_name, value, StringComparison.InvariantCultureIgnoreCase));
+            Func<string, bool> nameFilter = value => existingResults.Any(r => string.Equals(r.Data.FirstName, value, StringComparison.InvariantCultureIgnoreCase));
 
             // Query Input
             var entityType = request.EntityMetaData.EntityType;
@@ -130,14 +130,9 @@ namespace CluedIn.ExternalSearch.Providers.Crunchbase
             var response = client.ExecuteTaskAsync<PersonResponse>(request).Result;
 
             if (response.StatusCode == HttpStatusCode.OK)
-            {
                 if (response.Data != null)
                     foreach (var person in response.Data.Data.Items)
-                    {
-                        person.Properties.uuid = person.Uuid;
-                        yield return new ExternalSearchQueryResult<PersonProperties>(query, person.Properties);
-                    }
-            }
+                        yield return new ExternalSearchQueryResult<PersonItem>(query, person);
             else if (response.StatusCode == HttpStatusCode.NoContent || response.StatusCode == HttpStatusCode.NotFound)
                 yield break;
             else if (response.ErrorException != null)
@@ -172,7 +167,7 @@ namespace CluedIn.ExternalSearch.Providers.Crunchbase
         /// <returns>The clues.</returns>
         public override IEnumerable<Clue> BuildClues(ExecutionContext context, IExternalSearchQuery query, IExternalSearchQueryResult result, IExternalSearchRequest request)
         {
-            var resultItem = result.As<PersonProperties>();
+            var resultItem = result.As<PersonItem>();
 
             var code = this.GetOriginEntityCode(resultItem);
 
@@ -183,7 +178,7 @@ namespace CluedIn.ExternalSearch.Providers.Crunchbase
             try
             {
                 using (var client = new WebClient())
-                using (var stream = client.OpenRead(resultItem.Data.profile_image_url)) //Get Full Quality Image
+                using (var stream = client.OpenRead(resultItem.Data.Properties.ProfileImageUrl)) //Get Full Quality Image
                 {
                     var inArray = StreamUtilies.ReadFully(stream);
                     if (inArray != null)
@@ -208,12 +203,10 @@ namespace CluedIn.ExternalSearch.Providers.Crunchbase
                 //Swallow
             }
 
-
+            var c = clue.Serialize();
 
             yield return clue;
         }
-
-
 
         /// <summary>Gets the primary entity metadata.</summary>
         /// <param name="context">The context.</param>
@@ -222,7 +215,7 @@ namespace CluedIn.ExternalSearch.Providers.Crunchbase
         /// <returns>The primary entity metadata.</returns>
         public override IEntityMetadata GetPrimaryEntityMetadata(ExecutionContext context, IExternalSearchQueryResult result, IExternalSearchRequest request)
         {
-            var resultItem = result.As<PersonProperties>();
+            var resultItem = result.As<PersonItem>();
             return this.CreateMetadata(resultItem);
         }
 
@@ -239,7 +232,7 @@ namespace CluedIn.ExternalSearch.Providers.Crunchbase
         /// <summary>Creates the metadata.</summary>
         /// <param name="resultItem">The result item.</param>
         /// <returns>The metadata.</returns>
-        private IEntityMetadata CreateMetadata(IExternalSearchQueryResult<PersonProperties> resultItem)
+        private IEntityMetadata CreateMetadata(IExternalSearchQueryResult<PersonItem> resultItem)
         {
             var metadata = new EntityMetadataPart();
 
@@ -251,9 +244,9 @@ namespace CluedIn.ExternalSearch.Providers.Crunchbase
         /// <summary>Gets the origin entity code.</summary>
         /// <param name="resultItem">The result item.</param>
         /// <returns>The origin entity code.</returns>
-        private EntityCode GetOriginEntityCode(IExternalSearchQueryResult<PersonProperties> resultItem)
+        private EntityCode GetOriginEntityCode(IExternalSearchQueryResult<PersonItem> resultItem)
         {
-            return new EntityCode(EntityType.Person, this.GetCodeOrigin(), resultItem.Data.uuid);
+            return new EntityCode(EntityType.Infrastructure.User, this.GetCodeOrigin(), resultItem.Data.Uuid);
         }
 
         /// <summary>Gets the code origin.</summary>
@@ -263,37 +256,44 @@ namespace CluedIn.ExternalSearch.Providers.Crunchbase
             return CodeOrigin.CluedIn.CreateSpecific("crunchBase");
         }
 
-
-        private void PopulateMetadata(IEntityMetadata metadata, IExternalSearchQueryResult<PersonProperties> resultItem)
+        private void PopulateMetadata(IEntityMetadata metadata, IExternalSearchQueryResult<PersonItem> resultItem)
         {
             var code = this.GetOriginEntityCode(resultItem);
 
             metadata.EntityType = code.Type;
             metadata.OriginEntityCode = code;
 
-            if (resultItem.Data.also_known_as != null)
-                metadata.Aliases.AddRange(resultItem.Data.also_known_as);
+            metadata.Name = string.Format("{0} {1}", resultItem.Data.Properties.FirstName, resultItem.Data.Properties.LastName);
 
-            metadata.Name = string.Format("{0} {1}", resultItem.Data.first_name, resultItem.Data.last_name);
-            metadata.Description = resultItem.Data.bio.PrintIfAvailable();
-            metadata.Properties[CrunchbaseVocabulary.Person.BornOn] = resultItem.Data.born_on.PrintIfAvailable();
-            metadata.Properties[CrunchbaseVocabulary.Person.BornOnTrustCode] = resultItem.Data.born_on_trust_code.PrintIfAvailable();
-            metadata.Properties[CrunchbaseVocabulary.Person.CreatedAt] = resultItem.Data.created_at.PrintIfAvailable();
-            metadata.Properties[CrunchbaseVocabulary.Person.DiedOn] = resultItem.Data.died_on.PrintIfAvailable();
-            metadata.Properties[CrunchbaseVocabulary.Person.DiedOnTrustCode] = resultItem.Data.died_on_trust_code.PrintIfAvailable();
-            metadata.Properties[CrunchbaseVocabulary.Person.FirstName] = resultItem.Data.first_name.PrintIfAvailable();
-            metadata.Properties[CrunchbaseVocabulary.Person.Gender] = resultItem.Data.gender.PrintIfAvailable();
-            metadata.Properties[CrunchbaseVocabulary.Person.LastName] = resultItem.Data.last_name.PrintIfAvailable();
-            metadata.Properties[CrunchbaseVocabulary.Person.PermaLink] = resultItem.Data.permalink.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.OrganizationPermalink] = resultItem.Data.Properties.OrganizationPermalink?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.Permalink]             = resultItem.Data.Properties.Permalink?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.ApiPath]               = resultItem.Data.Properties.ApiPath?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.WebPath]               = resultItem.Data.Properties.WebPath?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.ApiUrl]                = resultItem.Data.Properties.ApiUrl?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.FirstName]             = resultItem.Data.Properties.FirstName?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.LastName]              = resultItem.Data.Properties.LastName?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.Gender]                = resultItem.Data.Properties.Gender?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.Title]                 = resultItem.Data.Properties.Title?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.OrganizationApiPath]   = resultItem.Data.Properties.OrganizationApiPath?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.OrganizationWebPath]   = resultItem.Data.Properties.OrganizationWebPath?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.OrganizationName]      = resultItem.Data.Properties.OrganizationName?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.ProfileImageUrl]       = resultItem.Data.Properties.ProfileImageUrl?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.HomepageUrl]           = resultItem.Data.Properties.HomepageUrl?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.FacebookUrl]           = resultItem.Data.Properties.FacebookUrl?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.TwitterUrl]            = resultItem.Data.Properties.TwitterUrl?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.LinkedinUrl]           = resultItem.Data.Properties.LinkedinUrl?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.CityName]              = resultItem.Data.Properties.CityName?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.RegionName]            = resultItem.Data.Properties.RegionName?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.CountryCode]           = resultItem.Data.Properties.CountryCode?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.CreatedAt]             = resultItem.Data.Properties.CreatedAt?.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.UpdatedAt]             = resultItem.Data.Properties.UpdatedAt?.PrintIfAvailable();
 
             //Download this picture
-            metadata.Properties[CrunchbaseVocabulary.Person.ProfileImageUrl] = resultItem.Data.profile_image_url.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.ProfileImageUrl] = resultItem.Data.Properties.ProfileImageUrl?.PrintIfAvailable();
 
-            metadata.Properties[CrunchbaseVocabulary.Person.RoleInvestor] = resultItem.Data.role_investor.PrintIfAvailable();
-            metadata.Properties[CrunchbaseVocabulary.Person.UpdatedAt] = resultItem.Data.updated_at.PrintIfAvailable();
+            metadata.Properties[CrunchbaseVocabulary.Person.UpdatedAt] = resultItem.Data.Properties?.UpdatedAt?.PrintIfAvailable();
         
             metadata.Codes.Add(code);
-
         }
     }
 }
--- src/Vocabularies/CrunchbasePersonVocabulary.cs
+++ src/Vocabularies/CrunchbasePersonVocabulary.cs
@@ -24,33 +24,64 @@ namespace CluedIn.ExternalSearch.Providers.Crunchbase.Vocabularies
             this.VocabularyName = "Crunchbase Person";
             this.KeyPrefix      = "crunchbase.person";
             this.KeySeparator   = ".";
-            this.Grouping       = EntityType.Person;
+            this.Grouping       = EntityType.Infrastructure.User;
 
-            this.BornOn = this.Add(new VocabularyKey("BornOn"));
-            this.BornOnTrustCode = this.Add(new VocabularyKey("BornOnTrustCode"));
-            this.CreatedAt = this.Add(new VocabularyKey("CreatedAt"));
-            this.DiedOn = this.Add(new VocabularyKey("DiedOn"));
-            this.DiedOnTrustCode = this.Add(new VocabularyKey("DiedOnTrustCode"));
-            this.FirstName = this.Add(new VocabularyKey("FirstName"));
-            this.Gender = this.Add(new VocabularyKey("Gender"));
-            this.LastName = this.Add(new VocabularyKey("LastName"));
-            this.PermaLink = this.Add(new VocabularyKey("PermaLink"));
-            this.ProfileImageUrl = this.Add(new VocabularyKey("ProfileImageUrl"));
-            this.RoleInvestor = this.Add(new VocabularyKey("RoleInvestor"));
-            this.UpdatedAt = this.Add(new VocabularyKey("UpdatedAt"));
+            this.OrganizationPermalink = this.Add(new VocabularyKey("organizationPermalink"));
+            this.Permalink             = this.Add(new VocabularyKey("permalink"));
+            this.ApiPath               = this.Add(new VocabularyKey("apiPath"));
+            this.WebPath               = this.Add(new VocabularyKey("webPath"));
+            this.ApiUrl                = this.Add(new VocabularyKey("apiUrl"));
+            this.FirstName             = this.Add(new VocabularyKey("firstName"));
+            this.LastName              = this.Add(new VocabularyKey("lastName"));
+            this.Gender                = this.Add(new VocabularyKey("gender"));
+            this.Title                 = this.Add(new VocabularyKey("title"));
+            this.OrganizationApiPath   = this.Add(new VocabularyKey("organizationApiPath"));
+            this.OrganizationWebPath   = this.Add(new VocabularyKey("organizationWebPath"));
+            this.OrganizationName      = this.Add(new VocabularyKey("organizationName"));
+            this.ProfileImageUrl       = this.Add(new VocabularyKey("profileImageUrl"));
+            this.HomepageUrl           = this.Add(new VocabularyKey("homepageUrl"));
+            this.FacebookUrl           = this.Add(new VocabularyKey("facebookUrl"));
+            this.TwitterUrl            = this.Add(new VocabularyKey("twitterUrl"));
+            this.LinkedinUrl           = this.Add(new VocabularyKey("linkedinUrl"));
+            this.CityName              = this.Add(new VocabularyKey("cityName"));
+            this.RegionName            = this.Add(new VocabularyKey("regionName"));
+            this.CountryCode           = this.Add(new VocabularyKey("countryCode"));
+            this.CreatedAt             = this.Add(new VocabularyKey("createdAt"));
+            this.UpdatedAt             = this.Add(new VocabularyKey("updatedAt"));
+
+            this.AddMapping(this.FirstName, CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInUser.FirstName);
+            this.AddMapping(this.LastName, CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInUser.LastName);
+            this.AddMapping(this.Gender, CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInUser.Gender);
+            this.AddMapping(this.HomepageUrl, CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInUser.Website);
+            this.AddMapping(this.FacebookUrl, CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInUser.SocialFacebook);
+            this.AddMapping(this.TwitterUrl, CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInUser.SocialTwitter);
+            this.AddMapping(this.LinkedinUrl, CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInUser.SocialLinkedIn);
+            this.AddMapping(this.CityName, CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInUser.HomeAddressCity);
+            this.AddMapping(this.CountryCode, CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInUser.HomeAddressCountryCode);
+            this.AddMapping(this.OrganizationName, CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInOrganization.OrganizationName);
         }
 
-        public VocabularyKey BornOn { get; internal set; }
-        public VocabularyKey BornOnTrustCode { get; internal set; }
-        public VocabularyKey CreatedAt { get; internal set; }
-        public VocabularyKey DiedOn { get; internal set; }
-        public VocabularyKey DiedOnTrustCode { get; internal set; }
-        public VocabularyKey FirstName { get; internal set; }
-        public VocabularyKey Gender { get; internal set; }
-        public VocabularyKey LastName { get; internal set; }
-        public VocabularyKey PermaLink { get; internal set; }
-        public VocabularyKey ProfileImageUrl { get; internal set; }
-        public VocabularyKey RoleInvestor { get; internal set; }
-        public VocabularyKey UpdatedAt { get; internal set; }
+        public VocabularyKey OrganizationPermalink { get; internal set; }
+        public VocabularyKey Permalink             { get; internal set; }
+        public VocabularyKey ApiPath               { get; internal set; }
+        public VocabularyKey WebPath               { get; internal set; }
+        public VocabularyKey ApiUrl                { get; internal set; }
+        public VocabularyKey FirstName             { get; internal set; }
+        public VocabularyKey LastName              { get; internal set; }
+        public VocabularyKey Gender                { get; internal set; }
+        public VocabularyKey Title                 { get; internal set; }
+        public VocabularyKey OrganizationApiPath   { get; internal set; }
+        public VocabularyKey OrganizationWebPath   { get; internal set; }
+        public VocabularyKey OrganizationName      { get; internal set; }
+        public VocabularyKey ProfileImageUrl       { get; internal set; }
+        public VocabularyKey HomepageUrl           { get; internal set; }
+        public VocabularyKey FacebookUrl           { get; internal set; }
+        public VocabularyKey TwitterUrl            { get; internal set; }
+        public VocabularyKey LinkedinUrl           { get; internal set; }
+        public VocabularyKey CityName              { get; internal set; }
+        public VocabularyKey RegionName            { get; internal set; }
+        public VocabularyKey CountryCode           { get; internal set; }
+        public VocabularyKey CreatedAt             { get; internal set; }
+        public VocabularyKey UpdatedAt             { get; internal set; }
     }
 }
